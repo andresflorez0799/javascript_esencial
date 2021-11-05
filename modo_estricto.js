f0c95fbdc95ad7d24707fcdc29668e0dffd0f492
andresflorez0799 <andresflorez0799@gmail.com>
2021-11-04 21:27:20 -0500

    Primer commit del proyecto

---
+++ modo_estricto.js
@@ -0,0 +1,24 @@
+'use strict';
+
+/**
+ * lo siguiente marcara error debido a que el modo estricto de javascript 
+ * nos obliga a respetar en este caso colocar la declaracion de la variable con 
+ * la palabra var, let o const
+ */
+//variable_sin_declarar = 'Hola desde javascript';
+//console.log(variable_sin_declarar);
+
+/**
+ * Esto si estaria permitido con el modo estricto
+ */
+let variable_con_let = "mi variable let";
+var variable_con_var = "mi variable var";
+const constante_con_const = "mi constante";
+
+/**
+ * Imprimir por consola los contenidos
+ */
+console.log(variable_con_let);
+console.log(variable_con_var);
+console.log(constante_con_const);
+
